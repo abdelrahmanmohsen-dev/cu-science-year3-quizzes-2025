9ad219690a13b170d53b799e3a04340f5ba9a987
Abdelrahman Mohsen Ahmed <10812023100314@stud.cu.edu.eg>
2025-12-14 14:29:16 +0200

    Add files via upload
    HuffmanAlgo

---
+++ Cs305-Algorithms/Chapter 4/HuffmanNode.java
@@ -0,0 +1,56 @@
+// Implements Comparable so PriorityQueue knows how to sort these nodes
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    char symbol;
+    int frequency;
+    HuffmanNode left;
+    HuffmanNode right;
+
+    // Constructor 1: For Leaf Nodes (actual characters)
+    public HuffmanNode(char symbol, int frequency) {
+        this.symbol = symbol;
+        this.frequency = frequency;
+    }
+
+    // Constructor 2: For Internal Nodes (combined sums)
+    public HuffmanNode(int frequency, HuffmanNode left, HuffmanNode right) {
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+        this.symbol = '-'; // Internal nodes don't have a specific character
+    }
+
+    // --- Getters and Setters ---
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public HuffmanNode getLeft() {
+        return left;
+    }
+
+    public HuffmanNode getRight() {
+        return right;
+    }
+
+    public void setRight(HuffmanNode right) {
+        this.right = right;
+    }
+
+    // --- Core Logic ---
+
+    // This defines the "Priority": Lower frequency = Higher priority (comes first)
+    @Override
+    public int compareTo(HuffmanNode o) {
+        return Integer.compare(this.frequency, o.frequency);
+    }
+
+    // Helper for debugging/printing
+    @Override
+    public String toString() {
+        return "Node{" + "char=" + symbol + ", freq=" + frequency + "}";
+    }
+}
